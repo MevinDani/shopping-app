8f47d68bcd7405c1054ac6ee0a486fc54edd5bf9
MEVIN DANI <you@example.com>
2021-06-01 12:25:12 +0530

    updated our application with reselect, created cart selectors with memoized selectors

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,4 @@
-import { CartActionTypes } from "./cart.types";
+import CartActionTypes from "./cart.types";
 import { addItemToCart } from "./cart.utils";
 
 const INITIAL_STATE = {
@@ -15,6 +15,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       };
     case CartActionTypes.ADD_ITEM:
       return {
+        ...state,
         cartItems: addItemToCart(state.cartItems, action.payload),
       };
     default:
