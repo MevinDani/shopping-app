3fd699a7c084478e2e04f9b45577e1969e84c6f4
MEVIN DANI <you@example.com>
2021-06-03 14:35:39 +0530

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -20,8 +20,8 @@ const CollectionItem = ({item,addItem}) => {
             </div>
             <CustomButton onClick={()=>addItem(item)} inverted>Add to cart</CustomButton>
         </div>
-    )
-}
+    );
+};
 
 const mapDispatchToProps = dispatch => ({
     addItem:item => dispatch(addItem(item))
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,22 @@
 import { combineReducers } from "redux";
-import  cartReducer  from "../redux/cart/cart.reducer";
+import cartReducer from "../redux/cart/cart.reducer";
 import userReducer from "./user/user.reducer";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducers";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
